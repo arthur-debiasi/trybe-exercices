7bdcd87e067041fba69fb66be44e6a5727556fb1
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-06-21 18:42:11 -0300

    exercício 2

--- 01-fundamentals/04-javascript-and-logic-programming-introduction/01-javascript-introduction/01-day-javascript-first-steps/scripts.js
+++ 01-fundamentals/04-javascript-and-logic-programming-introduction/01-javascript-introduction/01-day-javascript-first-steps/scripts.js
@@ -6,12 +6,23 @@
 // > Divisão (a / b)
 // > Módulo (a % b)
 
+// const a = 10;
+// const b = 5;
 
-const a = 10;
-const b = 5;
+// console.log('Soma: ' + (a + b));
+// console.log('Subtração: ' + (a - b));
+// console.log('Multiplicação: ' + (a * b));
+// console.log('Divisão: ' + (a / b));
+// console.log('Módulo: ' + (a % b));
 
-console.log('Soma: ' + (a + b));
-console.log('Subtração: ' + (a - b));
-console.log('Multiplicação: ' + (a * b));
-console.log('Divisão: ' + (a / b));
-console.log('Módulo: ' + (a % b));
+// 2. Faça um programa que retorne o maior de dois números.
+// Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const c = 3;
+const d = 5;
+
+if (c > d) {
+  console.log(c + " is greater then " + d);
+} else {
+  console.log(d + " is greater then " + c);
+}
