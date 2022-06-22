0bbe191f7c264751481be46c17b706361eefb118
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-06-22 18:48:57 -0300

    exercício 4

--- 01-fundamentals/04-introduction-to-javascript-and-logic-programming/02-day-javascript-array-and-loop-for/scripts.js
+++ 01-fundamentals/04-introduction-to-javascript-and-logic-programming/02-day-javascript-array-and-loop-for/scripts.js
@@ -19,6 +19,18 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // 3. Para o terceiro exercício, calcule e imprima a média 
 // aritmética dos valores contidos no array;
 
+// let sum = 0;
+// let index = 0;
+// for (let index = 0; index < numbers.length; index++) {
+//     sum += numbers[index];
+// }
+// let average = sum / numbers.length;
+// console.log("A média aritmética dos valores contidos em numbers é", average, ".");
+
+// 4. Com o mesmo código do exercício anterior, caso o valor final 
+// seja maior que 20, imprima a mensagem: "valor maior que 20". 
+// Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
 let sum = 0;
 let index = 0;
 for (let index = 0; index < numbers.length; index++) {
@@ -26,3 +38,9 @@ for (let index = 0; index < numbers.length; index++) {
 }
 let average = sum / numbers.length;
 console.log("A média aritmética dos valores contidos em numbers é", average, ".");
+
+if (average > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}
