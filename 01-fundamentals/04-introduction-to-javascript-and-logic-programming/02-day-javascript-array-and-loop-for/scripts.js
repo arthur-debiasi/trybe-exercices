c3ff898b161d041138f4c08eedb2734ad776bf5b
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-06-22 18:43:46 -0300

    exercício 3

--- 01-fundamentals/04-introduction-to-javascript-and-logic-programming/02-day-javascript-array-and-loop-for/scripts.js
+++ 01-fundamentals/04-introduction-to-javascript-and-logic-programming/02-day-javascript-array-and-loop-for/scripts.js
@@ -8,9 +8,21 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // 2. Para o segundo exercício, some todos os valores contidos no array
-//  e imprima o resultado;
-let sum = 0
+// //  e imprima o resultado;
+
+// let sum = 0
+// for (let index = 0; index < numbers.length; index++) {
+//     sum = sum + numbers[index];  
+// }
+// console.log(sum);
+
+// 3. Para o terceiro exercício, calcule e imprima a média 
+// aritmética dos valores contidos no array;
+
+let sum = 0;
+let index = 0;
 for (let index = 0; index < numbers.length; index++) {
-    sum = sum + numbers[index];  
+    sum += numbers[index];
 }
-console.log(sum);
+let average = sum / numbers.length;
+console.log("A média aritmética dos valores contidos em numbers é", average, ".");
