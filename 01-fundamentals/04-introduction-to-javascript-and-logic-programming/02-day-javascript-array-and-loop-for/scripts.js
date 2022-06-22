a100c6f8239fa9a69e5e59eadcc2973360ccfbb8
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-06-22 18:37:43 -0300

    exercício 2

--- 01-fundamentals/04-introduction-to-javascript-and-logic-programming/02-day-javascript-array-and-loop-for/scripts.js
+++ 01-fundamentals/04-introduction-to-javascript-and-logic-programming/02-day-javascript-array-and-loop-for/scripts.js
@@ -3,7 +3,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // 1. Nesse primeiro exercício, percorra o array imprimindo todos os 
 // valores nele contidos com a função console.log();
 
-for (let index = 0; index < numbers.length; index++) {
-    console.log(numbers[index]);    
-}
+// for (let index = 0; index < numbers.length; index++) {
+//     console.log(numbers[index]);    
+// }
 
+// 2. Para o segundo exercício, some todos os valores contidos no array
+//  e imprima o resultado;
+let sum = 0
+for (let index = 0; index < numbers.length; index++) {
+    sum = sum + numbers[index];  
+}
+console.log(sum);
