8cda6ccf6ecff15f2185851bc919778f708d622f
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-06-22 18:37:11 -0300

    exercício 1

---
+++ 01-fundamentals/04-introduction-to-javascript-and-logic-programming/02-day-javascript-array-and-loop-for/scripts.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// 1. Nesse primeiro exercício, percorra o array imprimindo todos os 
+// valores nele contidos com a função console.log();
+
+for (let index = 0; index < numbers.length; index++) {
+    console.log(numbers[index]);    
+}
+
