60aac75f8cb852771ac8ca7d0c7c56d7e9ac6aff
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-06-23 12:08:53 -0300

    bônus 3

--- 01-fundamentals/04-introduction-to-javascript-and-logic-programming/02-day-javascript-array-and-loop-for/scripts.js
+++ 01-fundamentals/04-introduction-to-javascript-and-logic-programming/02-day-javascript-array-and-loop-for/scripts.js
@@ -136,18 +136,40 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Bônus 2.  Ordene o array numbers em ordem decrescente e imprima seus valores;
 
-for (let i = 1; i < numbers.length; i++) {
+// for (let i = 1; i < numbers.length; i++) {
     
-    for (let j = 0; j < i ; j++) {
+//     for (let j = 0; j < i ; j++) {
        
-        if (numbers[i] > numbers[j]) {
-           let higher = numbers[i];
-           numbers[i] = numbers[j];
-           numbers[j] = higher;
+//         if (numbers[i] > numbers[j]) {
+//            let higher = numbers[i];
+//            numbers[i] = numbers[j];
+//            numbers[j] = higher;
        
-        }
+//         }
         
-    }
+//     }
     
+// }
+// console.log(numbers);
+
+// Bônus 3. Agora crie um novo array a partir do array numbers, sem perdê-lo.
+// Cada valor do novo array deverá ser igual ao valor correspondente no array 
+// numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo 
+// array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 
+// (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a
+// multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante.
+// Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça 
+// isso utilizando o for e o método push.
+
+let numbers2 = [];
+
+for (let i = 1; i <= numbers.length ; i += 1) {   
+   if (i != numbers.length) {
+    numbers2.push(numbers[i] * numbers[i - 1]);
+   } else {
+       numbers2.push(numbers[i - 1] * 2);
+   }
 }
-console.log(numbers);
+console.log(numbers2);
+
+// Qual o motivo de alguém querer fazer desse jeito? kkkkk
