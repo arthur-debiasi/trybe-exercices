8613dcd188202472481708663c3340e4c8aca15f
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-06-24 08:52:48 -0300

    exercício 2

--- 01-fundamentals/04-javascript-and-logic-programming/03-day-javascript-logic-programming-and-algoritms/scripts.js
+++ 01-fundamentals/04-javascript-and-logic-programming/03-day-javascript-logic-programming-and-algoritms/scripts.js
@@ -2,11 +2,24 @@
 
 // Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
 
-let n = 10;
-let fatorial = 1;
+// let n = 10;
+// let fatorial = 1;
 
-for (let i = 1; i <= n; i++) {
-    fatorial = fatorial * i;
+// for (let i = 1; i <= n; i++) {
+//     fatorial = fatorial * i;
+// }
+
+// console.log(fatorial);
+
+// 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana"
+//  seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para 
+//  verificar se seu algoritmo está funcionando corretamente.
+
+let word     = 'arthur debiasi';
+let reverseW = '';
+
+for (let i = word.length - 1; i >= 0 ; i -= 1) {
+    reverseW += word[i]    
 }
 
-console.log(fatorial);
+console.log(reverseW);
