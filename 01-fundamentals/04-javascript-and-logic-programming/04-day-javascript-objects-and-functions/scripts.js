8106056d213f70ca1a588d9ee30e58cda777646c
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-06-24 21:17:18 -0300

    exercício 2

--- 01-fundamentals/04-javascript-and-logic-programming/04-day-javascript-objects-and-functions/scripts.js
+++ 01-fundamentals/04-javascript-and-logic-programming/04-day-javascript-objects-and-functions/scripts.js
@@ -7,8 +7,21 @@ let info = {
 //   1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, 
 // incluindo seu nome. Valor esperado no console: Bem-vinda, Margarida
 
-function mensagem(nome) {
-  return console.log("Bem vinda, " + nome + ".");
-}
+// function mensagem(nome) {
+//   return console.log("Bem vinda, " + nome + ".");
+// }
 
-mensagem(info.personagem)
+// mensagem(info.personagem)
+
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente'
+//  e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+
+//  {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim'
+// }
+
+info.recorrente ='Sim';
+console.log(info);
