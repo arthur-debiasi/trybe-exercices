2a76485c4652577a0463faea28541c54bc19fc23
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-07-25 19:15:33 -0300

    exercicio 4

--- 01-fundamentals/08-higher-order-functions-es6-javascript/03-day-javascript-es6-higher-order-functions-map-and-filter/exercicios.js
+++ 01-fundamentals/08-higher-order-functions-es6-javascript/03-day-javascript-es6-higher-order-functions-map-and-filter/exercicios.js
@@ -162,3 +162,39 @@ const expectedResult3 = [
 const fantasyOrScienceFiction = (books) => books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
 
 console.log(fantasyOrScienceFiction(books));
+
+console.log('-=-=-=-=exercício 4 =-=-=-=-');
+
+// 🚀 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+// Dica: use as funções filter e sort
+
+const expectedResult4 = [
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+      releaseYear: 1928,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: { name: 'Isaac Asimov', birthYear: 1920 },
+      releaseYear: 1951,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+      releaseYear: 1954,
+    },
+  ];
+  
+const oldBooksOrdered = (books) => {
+    const olderBooks = books.filter((book) => (2022 - book.releaseYear) > 60);
+    return olderBooks.sort((bookA, bookB) => bookA.releaseYear - bookB.releaseYear);
+}
+
+console.log(oldBooksOrdered(books));
