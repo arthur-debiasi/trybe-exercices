db8089c25916b0613aceaad5b4fbd05876e6c023
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-07-26 15:45:53 -0300

    adiciona os exercícios de 1 a 7

---
+++ 01-fundamentals/08-higher-order-functions-es6-javascript/04-day-javascript-es6-higher-order-functions-reduce/exercice1.js
@@ -0,0 +1,11 @@
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+  ];
+  
+  // 1 - Dada uma matriz, transforme em um array.
+
+  function flatten() {
+    // escreva seu código aqui
+  }
---
+++ 01-fundamentals/08-higher-order-functions-es6-javascript/04-day-javascript-es6-higher-order-functions-reduce/exercice5.js
@@ -0,0 +1,11 @@
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
+  
+//   🚀 5- Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+  function containsA() {
+    // escreva seu código aqui
+  }
