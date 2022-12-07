c1fa0d04b0c8cc9a7b32c7c428412a82bbfc6697
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-12-07 08:50:10 -0300

    feat: bmi classification

--- 03-back-end-development/04-introducao-ao-desenvolvimento-web-com-node-js/01-node-js-runtime-assincrono/01-exercicio/bmi.js
+++ 03-back-end-development/04-introducao-ao-desenvolvimento-web-com-node-js/01-node-js-runtime-assincrono/01-exercicio/bmi.js
@@ -1,14 +1,51 @@
+const readLineSync = require('readline-sync');
+
 const bmiData = {
   weight: 80,
   height: 1.8
 }
 
-function bmi ({weight, height}) {
+function bmi () {
+  const height = readLineSync.questionFloat(`What is your height? `);
+  const weight = readLineSync.questionFloat(`What is your weight? `);
   return weight / (height ** 2);
 }
 
+function bmiClassification(bmi) {
+  let bmiClass = "MORBIDLY OBESE"
+  let healthRisk = 'EXTREMELY HIGH';
+  switch (true) {
+    case bmi < 18.5:
+      bmiClass = 'UNDERWEIGHT';
+      healthRisk = 'MINIMAL';
+      break;
+    case bmi < 25:
+      bmiClass = 'NORMAL';
+      healthRisk = 'MINIMAL';
+      break;
+    case bmi < 30:
+      bmiClass = 'OVERWEIGHT';
+      healthRisk = 'INCREASED';
+      break;
+    case bmi < 35:
+      bmiClass = 'OBESE';
+      healthRisk = 'HIGH';
+      break;
+    case bmi < 40:
+      bmiClass = 'SEVERELY OBESE';
+      healthRisk = 'VERY HIGH';
+      break;
+    default:
+      break;
+  }
+  return `Your BMI is: ${bmi.toFixed(2)}
+  Your BMI classification is : ${bmiClass}
+  Your Health Risk is : ${healthRisk}`
+} 
+
 function main() {
-  console.log(`BMI: ${bmi(bmiData).toFixed(2)}`);
+  const readBmi = bmi();
+  console.log(bmiClassification(readBmi));
 }
 
 main()
